c3cb6c5667a981b192d2f6ddecb6d62213aa62b2
Dejanvuk <dejan.vuk114@outlook.com>
2022-08-27 21:04:46 +0300

    ADDED: buildTreeSecondayMethod which builds the tree left->right

--- ds/Fenwick tree/Java/FenwickTree.java
+++ ds/Fenwick tree/Java/FenwickTree.java
@@ -1,5 +1,7 @@
 import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Deque;
+import java.util.List;
 
 /**
  * First attempt at building a Fenwick Tree
@@ -21,12 +23,13 @@ public class FenwickTree {
         }
     }
     public static void main(String[] args) {
-        int[] fenwick = {-5, 7, 0, 5, 3, 2, -1, 0, -2};
+        int[] fenwick = {4, 8 ,5, 2, 6, 1, 0, 8, 1, 5, 4, 9, 1, 0, 6, 6};
+        int[] fenwick2 = {1,2,4};
 
+        //Node root = buildTree(fenwick);
+        Node root2 = buildTreeSecondayMethod(fenwick, 0, fenwick.length - 1);
 
-        Node root = buildTree(fenwick);
-
-        printTree(root);
+        printTree(root2);
 
     }
 
@@ -44,11 +47,12 @@ public class FenwickTree {
 
         while(!dq.isEmpty()) {
             int width = dq.size();
-
+            final int widthCopy = width;
+            int i = 1;
             while(width-- > 0) {
                 Node polled = dq.poll();
 
-                System.out.print(polled.val + " ");
+                System.out.print("val: " + polled.val + " [i: " + i++ + "] ");
 
                 if(polled.left != null) {
                     dq.add(polled.left);
@@ -59,10 +63,41 @@ public class FenwickTree {
                 }
             }
 
-            System.out.println(" ");
+            System.out.println(" |||w: " + widthCopy + "||| ");
         }
     }
+
+    /**
+     * Helper function which converts an int[] array to a Collection of Node's
+     * @param fenwick
+     * @return
+     */
+    private static List<Node> arrToCollection (int[] fenwick) {
+        if(fenwick.length == 0) 
+            throw new IllegalArgumentException("Fenwick array cannot be empty!");
+
+        return Collections.emptyList();
+    }
     
+    /**
+     * Just another way to build up the tree, above on 'buildTree' we build it row by row, here we build it from left to right
+     * @param fenwick
+     * @return
+     */
+    private static Node buildTreeSecondayMethod (int[] fenwick, int l, int r) {
+        if(l == r) {
+            return new Node(fenwick[l]);
+        }
+
+        int mid = l + (r - l) / 2;
+
+        Node left = buildTreeSecondayMethod(fenwick, l, mid);
+        Node right = buildTreeSecondayMethod(fenwick, mid + 1, r);
+
+
+        return new Node(left, right, left.val + right.val);
+    }
+
     /**
      * @throws IllegalArgumentException
      * @param fenwick is the initial input
@@ -72,8 +107,6 @@ public class FenwickTree {
         if(fenwick.length == 0) 
             throw new IllegalArgumentException("Fenwick array cannot be empty!");
         
-        Node root = null;
-        //Deque<Integer> sums = new ArrayDeque<>();
         Deque<Node> nodes = new ArrayDeque<>();
 
         for(int val : fenwick) {
